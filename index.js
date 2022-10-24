7a5159bae99b27bc051c4440fb7970140332c147
nazmulrony <nhrony65@gmail.com>
2022-10-25 01:37:37 +0600

    server routes created for api calling

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
 
 app.get('/', (req, res) => {
     res.send('Courses server api site -by Nazmul Rony');
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '06') {
+        res.send(courses);
+    } else {
+        const selectedCategory = courses.filter(c => c.category_id === id);
+        res.send(selectedCategory);
+    }
+
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    selectedCourse = courses.find(c => c.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
