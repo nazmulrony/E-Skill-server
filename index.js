97510b216b02e467070df85b61a858cc8b15d4ba
nazmulrony <nhrony65@gmail.com>
2022-10-25 18:58:21 +0600

    cors middleware module enabled

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Courses server api site -by Nazmul Rony');
